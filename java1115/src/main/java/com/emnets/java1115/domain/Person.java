50ec07a179fae8b683788e19adb4c3256923a8d1
段瑶光 <1379606723@qq.com>
2022-11-23 11:38:57 +0800

    1123

--- java1115/src/main/java/com/emnets/java1115/domain/Person.java
+++ java1115/src/main/java/com/emnets/java1115/domain/Person.java
@@ -1,5 +1,10 @@
 package com.emnets.java1115.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Date;
+
 /**
  * @author lannisite
  * @program Person
@@ -11,6 +16,30 @@ public class Person {
     private String name;
     private int age;
     private String gender;
+    //@JsonIgnore
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date birthday;
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+
 
     public String getName() {
         return name;
@@ -36,12 +65,4 @@ public class Person {
         this.gender = gender;
     }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", gender='" + gender + '\'' +
-                '}';
-    }
 }
--- java1115/src/main/java/com/emnets/java1115/test/JacksonTest.java
+++ java1115/src/main/java/com/emnets/java1115/test/JacksonTest.java
@@ -2,11 +2,12 @@ package com.emnets.java1115.test;
 
 
 import com.emnets.java1115.domain.Person;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.*;
 
 /**
  * @author lannisite
@@ -32,8 +33,100 @@ public class JacksonTest {
         String json = mapper.writeValueAsString(p);
         System.out.println(json);
 
+        //  写入文件
         mapper.writeValue(new File("./a.txt"),p);
 
+        //  关联到writer中
+        mapper.writeValue(new FileWriter("./b.txt"),p);
+
     }
+
+
+    //  Java对象转为json相关的注解
+    @Test
+    public void test2() throws Exception {
+        Person p = new Person();
+        p.setName("dyg");
+        p.setAge(23);
+        p.setGender("男");
+        p.setBirthday(new Date());
+
+        //  2.创建Jackson的核心对象  ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        //  3.转换
+        String json = mapper.writeValueAsString(p);
+        System.out.println(json);
+    }
+
+
+    //  Java对象转为json  List
+    @Test
+    public void test3() throws Exception {
+        Person p = new Person();
+        p.setName("dyg");
+        p.setAge(23);
+        p.setGender("男");
+        p.setBirthday(new Date());
+
+        Person p1 = new Person();
+        p1.setName("gdf");
+        p1.setAge(23);
+        p1.setGender("男");
+        p1.setBirthday(new Date());
+
+        Person p2 = new Person();
+        p2.setName("fds");
+        p2.setAge(23);
+        p2.setGender("男");
+        p2.setBirthday(new Date());
+
+
+
+        List<Person> ps = new ArrayList<>();
+        ps.add(p);
+        ps.add(p1);
+        ps.add(p2);
+
+        //  2.创建Jackson的核心对象  ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        //  3.转换
+        String json = mapper.writeValueAsString(ps);
+        System.out.println(json);
+    }
+
+    //  map
+    @Test
+    public void test4() throws Exception {
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("name","zs");
+        map.put("age",23);
+        map.put("gender",true);
+
+
+        //  2.创建Jackson的核心对象  ObjectMapper
+        ObjectMapper mapper = new ObjectMapper();
+
+        //  3.转换
+        String json = mapper.writeValueAsString(map);
+        System.out.println(json);
+    }
+
+    @Test
+    public void test5() throws Exception {
+        //  1.
+        String json = "{\"gender\":true,\"name\":\"zs\",\"age\":23}";
+        //  2.
+        ObjectMapper mapper = new ObjectMapper();
+        //  3.
+        Person person = mapper.readValue(json, Person.class);
+        System.out.println(person);
+
+    }
+
+
+
 
 }
