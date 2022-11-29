ea8a451054fa10f12035e5a2505dfce296abfee4
段瑶光 <1379606723@qq.com>
2022-11-29 16:08:03 +0800

    1127

---
+++ java_maven/src/main/java/cn/emnets/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.emnets.demo;
+
+/**
+ * @author lannisite
+ * @program HelloWorld
+ * @description 描述
+ * @date 2022/11/23 22:03
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello maven");
+    }
+}
---
+++ java_maven/src/test/java/cn/emnets/test/TestHello.java
@@ -0,0 +1,19 @@
+package cn.emnets.test;
+
+import org.junit.Test;
+
+/**
+ * @author lannisite
+ * @program TestHello
+ * @description 描述
+ * @date 2022/11/23 22:13
+ */
+
+public class TestHello {
+
+    @Test
+    public void test1(){
+
+
+    }
+}
